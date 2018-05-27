46e5b9a4c94a271645e51db57d2b6d209f72e5ef
tehillaw <wittowtehilla@gmail.com>
2018-05-27 12:06:09 +0300

    Add protbuff

--- Server/server.py
+++ Server/server.py
@@ -2,24 +2,24 @@
 
 import socket
 
-TCP_IP = '192.168.43.114'
-TCP_PORT = 5007
-BUFFER_SIZE = 1024
+if __name__ == '__main__':
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
+    TCP_IP = '192.168.43.114'
+    TCP_PORT = 5007
+    BUFFER_SIZE = 1024
 
-conn, addr = s.accept()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
+    s.listen(1)
 
-print
-('Connection address:', addr)
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print
-    ('recieved data:', data)
-    data = data + '\0'
-    conn.send(data)
+    conn, addr = s.accept()
 
-conn.close()
+    print('Connection address:', addr)
+    while 1:
+        data = conn.recv(BUFFER_SIZE)
+        if not data: break
+        print('recieved data:', data)
+        data = data  # + '\0'.encode()
+        conn.send(data)
+
+    conn.close()
--- protocol/gen-proto.bat
+++ protocol/gen-proto.bat
@@ -1,7 +1,2 @@
-rem gen python proto
-protoc -I=. --python_out=../client data.proto
-protoc -I=. --python_out=../server data.proto
-
-rem gen cpp proto
-rem protoc -I=../ --cpp_out=../../client ../data.proto
-rem protoc -I=../ --cpp_out=../../server ../data.proto
+protoc -I=. --python_out=./server ratPacket.proto
+protoc -I=. --cpp_out=./client ratPacket.proto
